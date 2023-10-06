bddc2575cd23b285aae1cd78640e1b473f9607d2
Vaibhavi <vaibhavibhosale711@gmail.com>
2023-10-06 10:59:30 +0530

    toast error removed

--- client/src/Routes/Farmer/PaymentPage.js
+++ client/src/Routes/Farmer/PaymentPage.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { Link, useNavigate, useParams,useLocation } from "react-router-dom";
 import { useEffect } from 'react';
 import { useState } from 'react';
@@ -314,7 +316,7 @@ const confirmBooking = async (e) => {
         }
       },
       theme: {
-        color: "#3399cc",
+        color: "#3399cc",       
       },
     };
   };
